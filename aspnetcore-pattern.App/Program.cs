992701be011cabe31c5d6564a3b426e7ede9dd4c
Artur Ramme <artur.ramme@operacao.rcadigital.com.br>
2019-08-07 13:29:07 -0300

    circuit breaker with Polly

--- aspnetcore-pattern.App/Program.cs
+++ aspnetcore-pattern.App/Program.cs
@@ -8,40 +8,9 @@ namespace aspnetcore_pattern.App
     {
         static void Main(string[] args)
         {
-            var cb = new CircuitBreaker.CircuitBreaker(5, 3000);
-            try
-            {
-                int count = 0;
-                cb.Execute(() =>
-                {   
-                    try
-                    {
-                        if (count >= 4)
-                        {
-                            Console.WriteLine("OK");
-                        }
-                        else
-                        {
-                            throw new Exception();
-                        }
-                    }
-                    catch (Exception)
-                    {
-                        count++;
-                        throw;
-                    }                    
-                });
-            }
-            catch (CircuitBreakerOperationException ex)
-            {
-                Trace.Write(ex);
-            }
-            catch (OpenCircuitException openEx)
-            {
-                Console.Write(cb.IsOpen);
-            }
-            Console.Write(cb.IsClosed);
-            Console.Read();
+            var cb = new CircuitBreakerWithPolly();
+
+            cb.Execute();
         }
     }
 }
---
+++ aspnetcore-pattern.CircuitBreaker/CircuitBreakerWithPolly.cs
@@ -0,0 +1,49 @@
+﻿using Polly;
+using Polly.CircuitBreaker;
+using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+
+namespace aspnetcore_pattern.CircuitBreaker
+{
+    public class CircuitBreakerWithPolly
+    {
+        public void Execute()
+        {
+            var retry = Policy.Handle<WebException>().WaitAndRetryForever(attemp => TimeSpan.FromMilliseconds(300));
+            var circuitBreakerPolicy = Policy.Handle<WebException>().CircuitBreaker(3, TimeSpan.FromSeconds(15), onBreak: (ex, timespan, context) =>
+            {
+                Console.WriteLine("Circuito entrou em estado de falha");
+            }, onReset: (context) =>
+            {
+                Console.WriteLine("Circuito saiu do estado de falha");
+            });
+
+            while (true)
+            {
+                retry.Execute(() =>
+                {
+                    if (circuitBreakerPolicy.CircuitState != CircuitState.Open)
+                    {
+                        circuitBreakerPolicy.Execute(() =>
+                        {
+
+                            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://localhost");
+
+                            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                            using (Stream stream = response.GetResponseStream())
+                            using (StreamReader reader = new StreamReader(stream))
+                            {
+                                var html = reader.ReadToEnd();
+                                Console.WriteLine("Requisição feita com sucesso");
+                                Thread.Sleep(300);
+                            }
+
+                        });
+                    }
+                });
+            }
+        }
+    }
+}
